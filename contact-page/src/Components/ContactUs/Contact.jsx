a1c2155c75ec267de1e81c16ddb0dba7b3a4d488
Owais <zaidalit00@gmail.com>
2024-03-09 12:39:36 +0530

    Made main components

---
+++ contact-page/src/Components/ContactUs/Contact.jsx
@@ -0,0 +1,14 @@
+import './Contact.css'
+
+const ContactUs = () => {
+    return (
+        <>
+            <div className="container">
+                <h1>Contact Us</h1>
+                <p className='C-para'>Lets Connect, we are here to help and we would love to hear from you whether you have a question,comment, or just want to chat . You can reach out to us through the contact form of this page or by phone, email, or social media</p>
+            </div>
+        </>
+    )
+}
+
+export default ContactUs
